c8b95c9dafeaffc8f2a072d0b77ab28fec44ebae
rodrigosampler <rodrigoufcquixada@gmail.com>
2017-07-18 09:06:33 -0300

    concertando bootstrap

--- src/main/java/br/ufc/festa/controller/ConvidadosController.java
+++ src/main/java/br/ufc/festa/controller/ConvidadosController.java
@@ -2,6 +2,7 @@ package br.ufc.festa.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,6 +29,11 @@ public class ConvidadosController {
 	public String salvar(Convidado convidado) {
 	this.convidados.save(convidado);
 	return "redirect:/convidados";
+	}
+	@DeleteMapping
+	public String deletar(Convidado convidado){
+		this.convidados.delete(convidado);
+		return "redirect:/ convidados";
 	}
 	
 }
